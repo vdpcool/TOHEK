c445a45d71a38c04c8346c87dbecb397e7f8c441
K <138153603+vdpcool@users.noreply.github.com>
2023-08-27 15:48:13 +0700

    Add files via upload

---
+++ Utilities/MapUtilities.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Il2CppSystem;
+
+namespace TheOtherRoles.Utilities;
+
+public static class MapUtilities
+{
+    public static ShipStatus CachedShipStatus = ShipStatus.Instance;
+
+    public static void MapDestroyed()
+    {
+        CachedShipStatus = ShipStatus.Instance;
+        _systems.Clear();
+    }
+
+    private static readonly Dictionary<SystemTypes, Object> _systems = new();
+    public static Dictionary<SystemTypes, Object> Systems
+    {
+        get
+        {
+            if (_systems.Count == 0) GetSystems();
+            return _systems;
+        }
+    }
+
+    private static void GetSystems()
+    {
+        if (!CachedShipStatus) return;
+
+        var systems = CachedShipStatus.Systems;
+        if (systems.Count <= 0) return;
+        
+        foreach (var systemTypes in SystemTypeHelpers.AllTypes)
+        {
+            if (!systems.ContainsKey(systemTypes)) continue;
+            _systems[systemTypes] = systems[systemTypes].TryCast<Object>();
+        }
+    }
+}
